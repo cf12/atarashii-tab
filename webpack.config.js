0ec8b1e3a11aab3bbf31652b2910264be7c5569e
CF12 <brianx0011@gmail.com>
2021-06-01 01:37:01 -0400

    Init webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/manifest.json' }
+      ]
+    })
+  ]
+}
