dcfd1122d82919399315e09267a13413dda16ef6
CF12 <brianx0011@gmail.com>
2021-06-05 00:06:01 -0400

    Implement no data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { decode } from "html-entities"
-import { FaReddit, FaCog } from "react-icons/fa"
+import { FaReddit, FaCog, FaSadTear } from "react-icons/fa"
 import { PuffLoader, RotateLoader } from "react-spinners"
 
-import useLocalStorage from './hooks/useLocalStorage'
+import useLocalStorage from "./hooks/useLocalStorage"
 
 import TimeDate from "./components/TimeDate"
 import ConfigModal from "./components/ConfigModal"
@@ -11,43 +11,45 @@ import Config from "./components/Config"
 import Icons from "./components/Icons"
 import Image from "./components/Image"
 
-import AppContext from './contexts/AppContext'
+import AppContext from "./contexts/AppContext"
 
 import "./App.scss"
 
 export default () => {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState(undefined)
   const [loaded, setLoaded] = useState(false)
 
   const [modalOpen, setModalOpen] = useState(false)
-  const [ config, setConfig ] = useLocalStorage('config', {
+  const [config, setConfig] = useLocalStorage("config", {
     theme: {
-      primary: '#ffc400'
+      primary: "#ffc400",
     },
-    sort: 'top',
-    t: 'all',
+    sort: "top",
+    t: "all",
   })
 
-  const [ cache, setCache ] = useLocalStorage('cache', {
+  const [cache, setCache] = useLocalStorage("cache", {
     lastUpdated: -1,
-    data: []
+    data: [],
   })
 
   useEffect(() => {
-    document.documentElement.style.setProperty('--primary', config.theme.primary)
+    document.documentElement.style.setProperty(
+      "--primary",
+      config.theme.primary
+    )
   }, [config])
 
   useEffect(() => {
-    if (!cache)
-      return
+    if (!cache) return
 
-    async function run () {
+    async function run() {
       let posts = []
 
       console.log(config)
 
       // Cache for 12 hours
-      if ((Date.now() - cache.lastUpdated) < (1000 * 60 * 60 * 12)) {
+      if (Date.now() - cache.lastUpdated < 1000 * 60 * 60 * 12) {
         posts = cache.data
       } else {
         let after = null
@@ -59,25 +61,32 @@ export default () => {
             t: config.t,
             show: "all",
             restrict_sr: 1,
-            after
+            after,
           })
 
-          const res = await fetch(`https://www.reddit.com/r/Animewallpaper/search.json?${query}`)
+          const res = await fetch(
+            `https://www.reddit.com/r/Animewallpaper/search.json?${query}`
+          )
           const json = await res.json()
 
           after = json.data.after
-          if (!after)
-            break
+          if (!after) break
 
           // Collect posts w/ i.redd.it only
           let tmpPosts = json.data.children
-            .map(e => e.data)
-            .filter(e => e.url.includes('i.redd.it'))
+            .map((e) => e.data)
+            .filter((e) => e.url.includes("i.redd.it"))
 
           posts = posts.concat(tmpPosts)
         }
 
         setCache({ lastUpdated: Date.now(), data: posts })
+      }
+
+      if (!posts.length) {
+        setData(null)
+        setLoaded(true)
+        return
       }
 
       const num = Math.floor(Math.random() * posts.length)
@@ -95,9 +104,7 @@ export default () => {
       parts = parts.map((e) => e.slice(1, -1))
       title = '"' + title.slice(0, cutoff).trim() + '"'
 
-      let resolution = parts.filter((e) =>
-        e.match(/[\d\s]+[x×*][\d\s]+/g)
-      )?.[0]
+      let resolution = parts.filter((e) => e.match(/[\d\s]+[x×*][\d\s]+/g))?.[0]
 
       if (resolution) {
         parts.splice(parts.indexOf(resolution), 1)
@@ -111,7 +118,7 @@ export default () => {
         res: resolution || "",
         url: post.url,
         link,
-        num
+        num,
       })
     }
 
@@ -121,13 +128,17 @@ export default () => {
   return (
     <AppContext.Provider
       value={{
-        data, setData,
-        cache, setCache,
-        config, setConfig,
-        loaded, setLoaded
+        data,
+        setData,
+        cache,
+        setCache,
+        config,
+        setConfig,
+        loaded,
+        setLoaded,
       }}
     >
-      <div className={loaded ? 'load' : ''}>
+      <div className={loaded ? "load" : ""}>
         <div className="content">
           <header>
             <div className="header-left">
@@ -150,15 +161,30 @@ export default () => {
           <footer className="to-bottom">
             <div className="attr">
               <p className="attr-from to-load to-delay-3">
-                Image from{" "}
-                <a href="https://reddit.com/r/animewallpaper">
-                  <FaReddit size={20} /> r/Animewallpaper
-                </a>
+                {data === null ? (
+                  <strong>
+                    No images found <FaSadTear size={20} />
+                  </strong>
+                ) : (
+                  <>
+                    Image from{" "}
+                    <a href="https://reddit.com/r/animewallpaper">
+                      <FaReddit size={20} /> r/Animewallpaper
+                    </a>
+                  </>
+                )}
               </p>
 
               <p className="attr-bottom to-load to-delay-4">
-                Post <strong>#{data?.num + 1}</strong> of <strong>{cache.data.length}</strong> •{" "}
-                <a href={data?.link}>{data?.link}</a>
+                {data === null ? (
+                  <>Try different filters! • Reddit down perhaps?</>
+                ) : (
+                  <>
+                    Post <strong>#{data?.num + 1}</strong> of{" "}
+                    <strong>{cache.data.length}</strong> •{" "}
+                    <a href={data?.link}>{data?.link}</a>
+                  </>
+                )}
               </p>
 
               {!loaded && (
@@ -170,18 +196,21 @@ export default () => {
 
             <div className="credits to-right">
               <p>
-                Created with &lt;3 • <a href="https://github.com/cf12/atarashii-tab">@cf12</a>
+                Created with &lt;3 •{" "}
+                <a href="https://github.com/cf12/atarashii-tab">@cf12</a>
               </p>
             </div>
           </footer>
         </div>
 
-        <Image
-          className="bg to-load-bg"
-          src={data?.url}
-          alt=""
-          onLoad={() => setLoaded(true)}
-        />
+        {data === null ? null : (
+          <Image
+            className="bg to-load-bg"
+            src={data?.url}
+            alt=""
+            onLoad={() => setLoaded(true)}
+          />
+        )}
       </div>
     </AppContext.Provider>
   )
