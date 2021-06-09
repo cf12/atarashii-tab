a1f6ae2a9f2e14df30b706a7557faa4e41e25fab
CF12 <brianx0011@gmail.com>
2021-06-09 17:23:08 -0400

    Use package.json description & version

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require("path")
+const fs = require("fs")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin")
+
+const package = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "package.json"))
+)
 
 module.exports = {
   entry: "./src/index.js",
@@ -17,13 +22,24 @@ module.exports = {
     new HtmlWebpackInlineSVGPlugin(),
     new CopyWebpackPlugin({
       patterns: [
-        { from: "./src/manifest.json" },
         { from: "./src/icons/icon-128.png", to: "icon-128.png" },
         { from: "./src/icons/icon-48.png", to: "icon-48.png" },
-        { from: "./src/icons/icon-16.png", to: "icon-16.png" }
+        { from: "./src/icons/icon-16.png", to: "icon-16.png" },
+        {
+          from: "./src/manifest.json",
+          transform: (content, path) => {
+            return Buffer.from(
+              JSON.stringify({
+                description: package.description,
+                version: package.version,
+                ...JSON.parse(content.toString()),
+              })
+            )
+          },
+        },
       ],
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
@@ -33,11 +49,16 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.m?js$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         use: {
           loader: "babel-loader",
           options: {
@@ -48,7 +69,5 @@ module.exports = {
     ],
   },
   // https://stackoverflow.com/questions/61462586
-  devtool: process.env.NODE_ENV === 'development'
-    ? 'inline-source-map'
-    : false
+  devtool: process.env.NODE_ENV === "development" ? "inline-source-map" : false,
 }
